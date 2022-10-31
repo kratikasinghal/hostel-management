6eeee8c5e997a55e88c5490a668547ce229f9a4f
kratika <kratika.singhal10@gmail.com>
2022-11-01 00:08:41 +0530

    changes in login/signup ui

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,17 @@
 import Signup from "./signup";
 import Login from "./login";
+import ResidentScreen from "./Screens./residentScreen.js"
 import {Route,Routes} from 'react-router-dom';
 function App() {
   
   return(
-    
      <div className="app">
-     <Routes>
+      <h1>hi</h1>
+     {/* <Routes>
      <Route path="/login" exact element={<Login/>}/>
         <Route path="/signup" exact element={<Signup/>}/>
-     </Routes>
-        
-        
-    </div>
+     </Routes> */}
+   </div>
     
     );
   
