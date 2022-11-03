95823bd8d509f0c9260d448eb0307d0519f037b9
Isha Agarwal <ishaagarwal772000@gmail.com>
2022-11-03 19:44:39 +0530

    Update profile integration

--- frontend/src/components/Message.js
+++ frontend/src/components/Message.js
@@ -1,17 +1,17 @@
 import Snackbar from "@mui/material/Snackbar";
 import Alert from "@mui/material/Alert";
 import React from "react";
-import { useState, useEffect } from "react";
+import { useState} from "react";
 
 const Message = props => {
-  const [open, setOpen] = useState("false");
+  const [open, setOpen] = useState(props.open)
   const handleClose = () => {
-    setOpen(false);
+    setOpen(prev => !prev)
   };
 
-  useEffect(() => {
-    if (props) setOpen(true);
-  }, [props, setOpen]);
+  // useEffect(() => {
+  //   if (props) setOpen(prevOpen => !prevOpen);
+  // }, [props, setOpen]);
   return (
     <Snackbar autoHideDuration={3000} open={open} onClose={handleClose}>
       <Alert
@@ -24,5 +24,9 @@ const Message = props => {
     </Snackbar>
   );
 };
+
+Message.defaultProps = {
+  open: false
+}
 
 export default Message;
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,16 +1,18 @@
 import thunk from "redux-thunk"
 import { configureStore } from '@reduxjs/toolkit'
-import { userRegisterReducer,userLoginReducer } from "./reducers/userReducers"
+import { userRegisterReducer,userLoginReducer,updateUserProfileReducer,userDetailsReducer } from "./reducers/userReducers"
 
 const reducer =  {
     userRegister: userRegisterReducer,
-    userLogin: userLoginReducer
+    userLogin: userLoginReducer,
+    updateUserProfile: updateUserProfileReducer,
+    userDetails: userDetailsReducer
 }
 
-// const userInfoFromStorage = localStorage.getItem('userInfo') ? JSON.parse(localStorage.getItem('userInfo')) : null
+const userInfoFromStorage = localStorage.getItem('userInfo') ? JSON.parse(localStorage.getItem('userInfo')) : null
 
 const initialState = {
-
+    userLogin: { userInfo: userInfoFromStorage }
 }
 
 const middleware = [thunk]
